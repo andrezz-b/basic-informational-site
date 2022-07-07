316cd6e8509b311bf479c19229b82a38d0f58ea9
Andrej Božić <abozic389@gmail.com>
2022-07-07 12:44:43 +0200

    Rewritten with express.

--- index.js
+++ index.js
@@ -1,41 +1,25 @@
-const http = require("http");
-const fs = require("fs/promises");
-const path = require("path");
+const express = require("express");
+const app = express();
+const path = require("node:path");
 
-const host = "localhost";
-const port = 8000;
+const port = 3000;
 
-const requestListener = async (req, res) => {
-  let filePath;
-  switch (req.url) {
-    case "/":
-      filePath = path.join(__dirname, "index.html");
-      break;
-    case "/about":
-      filePath = path.join(__dirname, "about.html");
-      break;
-    case "/contact-me":
-      filePath = path.join(__dirname, "contact-me.html");
-      break;
-    default:
-      filePath = path.join(__dirname, "404.html")
-      break;
-  }
-  
-  try {
-    const content = await fs.readFile(filePath);
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.end(content);
-  } catch (error) {
-    res.statusCode = 500;
-    res.end(error);
-    return;
-  }
-};
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-const server = http.createServer(requestListener);
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "about.html"));
+});
 
-server.listen(port, host, () => {
-  console.log(`Server is running on http://${host}:${port}`);
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname, "contact-me.html"));
+});
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "404.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Is alive on port ${port}`);
 });
